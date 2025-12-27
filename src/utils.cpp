261f9f287d4ac29d5817cba33641facb96467d6e
Harry Bevins <harrytjbevins@yahoo.co.uk>
2025-12-27 19:25:50 +0000

    adding comment to saveAllBooks in utils

--- src/utils.cpp
+++ src/utils.cpp
@@ -6,6 +6,11 @@
 using namespace std;
 
 void saveAllBooks(const vector<Book> books, const string homeDir) {
+  /* Saves all books to the database file, overwriting existing data.
+   * Params:
+   *   books - vector of Book structs to save
+   *   homeDir - path to the user's home directory
+   */
   ofstream out(homeDir + "/.book/books.txt", ios::trunc);
   for (Book b : books) {
     out << b.id << "|" << b.title << "|" << b.author << "|" << b.dayStarted
