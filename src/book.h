4a69da435be581a14788b507854056680743ece4
Harry Bevins <harrytjbevins@yahoo.co.uk>
2026-01-01 15:58:53 +0000

    formatting and adding a comment to the book.h

--- src/book.h
+++ src/book.h
@@ -1,18 +1,22 @@
+/* Definition of the Book structure to represent book information.
+ *
+ * Author: Harry Bevins
+ */
 #ifndef BOOK_H
 #define BOOK_H
 
 #include <string>
 
 struct Book {
-    int id;
-    std::string title;
-    std::string author;
-    std::string dayStarted;
-    std::string dayCompleted;
-    std::string dayAdded;
-    std::string status;
-    std::string rating;
-    std::string notes;
+  int id;
+  std::string title;
+  std::string author;
+  std::string dayStarted;
+  std::string dayCompleted;
+  std::string dayAdded;
+  std::string status;
+  std::string rating;
+  std::string notes;
 };
 
 #endif
